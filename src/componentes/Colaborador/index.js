c25b92a57a89eaa380613e92d389cd0353ead684
LuisAngelPonceAlvarez <poncealvarezluisangel4@gmail.com>
2023-04-07 08:00:47 -0600

    quito warning

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,25 +1,32 @@
-import "./Colaborador.css"
-import { AiFillCloseCircle, AiOutlineHeart, AiFillHeart } from "react-icons/ai"
+import "./Colaborador.css";
+import { AiFillCloseCircle, AiOutlineHeart, AiFillHeart } from "react-icons/ai";
 
 const Colaborador = (props) => {
-    const { nombre, puesto, foto, equipo, id, fav } = props.datos
-    const { colorPrimario, eliminarColaborador, like } = props
+  const { nombre, puesto, foto, id, fav } = props.datos;
+  const { colorPrimario, eliminarColaborador, like } = props;
 
-    // condicion ? verdadero : falso
+  // condicion ? verdadero : falso
 
-    return <div className="colaborador">
-        <AiFillCloseCircle className="eliminar" onClick={() => eliminarColaborador(id)} />
-        <div className="encabezado" style={{ backgroundColor: colorPrimario }}>
-            <img src={foto} alt={nombre} />
-        </div>
-        <div className="info">
-            <h4>{nombre}</h4>
-            <h5>{puesto}</h5>
-            {fav ? <AiFillHeart color="red" onClick={() => like(id)} /> : <AiOutlineHeart onClick={() => like(id)} />}
-
-
-        </div>
+  return (
+    <div className="colaborador">
+      <AiFillCloseCircle
+        className="eliminar"
+        onClick={() => eliminarColaborador(id)}
+      />
+      <div className="encabezado" style={{ backgroundColor: colorPrimario }}>
+        <img src={foto} alt={nombre} />
+      </div>
+      <div className="info">
+        <h4>{nombre}</h4>
+        <h5>{puesto}</h5>
+        {fav ? (
+          <AiFillHeart color="red" onClick={() => like(id)} />
+        ) : (
+          <AiOutlineHeart onClick={() => like(id)} />
+        )}
+      </div>
     </div>
-}
+  );
+};
 
-export default Colaborador
+export default Colaborador;
--- src/componentes/Equipo/index.js
+++ src/componentes/Equipo/index.js
@@ -4,7 +4,7 @@ import hexToRgba from 'hex-to-rgba';
 
 const Equipo = (props) => {
     //Destructuracion
-    const { colorPrimario, colorSecundario, titulo, id } = props.datos
+    const { colorPrimario, titulo, id } = props.datos
     const { colaboradores, eliminarColaborador, actualizarColor, like } = props
     const obj = {
         backgroundColor: hexToRgba(colorPrimario, 0.6)
